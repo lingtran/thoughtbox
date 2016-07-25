527b18d1b8f27a944d22182d96d53d13ac406d01
lingtran <lxiutran@gmail.com>
2016-07-25 16:58:04 -0600

    Pass feature test for user to edit link

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -13,4 +13,18 @@ class LinksController < ApplicationController
     redirect_to user_links_path(current_user)
   end
 
+  def update
+    @link = Link.find_by(user_id: current_user, id: params[:id])
+    if @link.update(link_params)
+      redirect_to user_links_path(current_user)
+    else
+      redirect_to edit_user_link_path(current_user, @link)
+    end
+  end
+
+  private
+    def link_params
+      params.require(:link).permit(:title, :url)
+    end
+
 end
