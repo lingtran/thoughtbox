cd83b52a926314ddaa3ed7ef0f4bd0045ff71e82
lingtran <lxiutran@gmail.com>
2016-08-02 16:26:36 -0600

    Implement sorting of list
    List of links can sort but buttons go away. Future iteration will include buttons and ability to edit links

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      get '/users/:user_id/links', to: 'links#index'
       patch '/users/:user_id/links/:link_id', to: 'links#update'
     end
   end
